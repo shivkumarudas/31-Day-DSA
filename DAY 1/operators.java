db101e71f9fbd935a5cc06c025f8b58da76e2965
shivkumar udas <shivkumarudas07@gmail.com>
2026-07-01 14:19:35 +0530

    Initial commit

---
+++ DAY 1/operators.java
@@ -0,0 +1,12 @@
+public class operators {
+    public static void main(String[] args){
+        int a=10;
+        int b=3;
+        System.out.println("a+b = "+(a+b));
+        System.out.println("a-b = "+(a-b));
+        System.out.println("a*b = "+(a*b));
+        System.out.println("a/b = "+(a/b));
+        System.out.println("a%b = "+(a%b));
+
+    }
+}
