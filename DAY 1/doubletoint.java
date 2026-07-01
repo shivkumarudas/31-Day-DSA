5afa57cb7450c7ff06c7a1b9f0b3ed866e8a855e
shivkumar udas <shivkumarudas07@gmail.com>
2026-07-01 22:13:30 +0530

    Create doubletoint.java

---
+++ DAY 1/doubletoint.java
@@ -0,0 +1,8 @@
+public class doubletoint {
+    public static void main(String[] args){
+        double num=99.99;
+        int a= (int) num;
+        System.out.println("The integer value is: " + a);
+        
+    }
+}
