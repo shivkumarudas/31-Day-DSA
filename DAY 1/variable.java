03a8e3908314cc1539b6f6c3eb4775236479f82c
shivkumar udas <shivkumarudas07@gmail.com>
2026-07-01 22:13:42 +0530

    Create variable.java

---
+++ DAY 1/variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[] args){
+        int age=20;
+        int height=165;
+        boolean student =true;
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(student);}
+}
