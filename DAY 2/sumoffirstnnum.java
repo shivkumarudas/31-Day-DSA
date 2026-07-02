03478d160a04348aefac8c1ddd9cf145407b1cbf
shivkumar udas <shivkumarudas07@gmail.com>
2026-07-02 22:46:25 +0530

    Create sumoffirstnnum.java

---
+++ DAY 2/sumoffirstnnum.java
@@ -0,0 +1,11 @@
+public class sumoffirstnnum {
+public static void main(String[] args){
+    int n=10;
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        sum=sum+i;
+        
+    }
+    System.out.println("The sum of first "+n+" numbers is "+sum);
+}    
+}
