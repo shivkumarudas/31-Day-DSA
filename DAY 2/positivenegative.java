f35f72d5cd0021c195582be2ebf4fe3e8c7a6cc4
shivkumar udas <shivkumarudas07@gmail.com>
2026-07-02 22:46:44 +0530

    Create positivenegative.java

---
+++ DAY 2/positivenegative.java
@@ -0,0 +1,14 @@
+public class positivenegative{
+    public static void main(String[] args){
+        int a=11;
+        if(a>0){
+            System.out.println("The number is positive");
+        }
+        else if(a<0){
+            System.out.println("The number is negative");
+        }
+        else{
+            System.out.println("The number is zero");
+        }
+    }
+}
