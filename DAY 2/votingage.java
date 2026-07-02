a25f0f98589106bbc6c711d43c0854ce858fdbed
shivkumar udas <shivkumarudas07@gmail.com>
2026-07-02 22:46:31 +0530

    Create votingage.java

---
+++ DAY 2/votingage.java
@@ -0,0 +1,14 @@
+
+
+public class votingage {
+    public static void main(String[] args){
+        int age=22;{
+        if(age>=18){
+            System.out.println("you can vote");
+        }
+        else{
+            System.out.println("you cannot vote");}
+        }
+        }
+    }
+
